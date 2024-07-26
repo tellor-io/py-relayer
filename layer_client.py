b75cacfb3740b65c267551789172778a16ee07b7
tkernell <tpkernell@gmail.com>
2024-07-26 12:06:32 -0500

    layer client error handling

--- layer_client.py
+++ layer_client.py
@@ -10,89 +10,89 @@ def strip_0x(value):
     return value
 
 # validator set functions
-def get_validator_timestamp_by_index(index):
+def get_validator_timestamp_by_index(index) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_validator_timestamp_by_index/{index}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting validator timestamp by index: {e}")
-        return None
+        return None, e
 
-def get_validator_checkpoint_params(timestamp):
+def get_validator_checkpoint_params(timestamp) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_validator_checkpoint_params/{timestamp}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting validator checkpoint params: {e}")
-        return None
+        return None, e
 
-def get_valset_by_timestamp(timestamp):
+def get_valset_by_timestamp(timestamp) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_valset_by_timestamp/{timestamp}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting validator set by timestamp: {e}")
-        return None
+        return None, e
 
-def get_valset_sigs(timestamp):
+def get_valset_sigs(timestamp) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_valset_sigs/{timestamp}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting validator set sigs: {e}")
-        return None
+        return None, e
 
-def get_current_validator_set_timestamp():
+def get_current_validator_set_timestamp() -> (str, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_current_validator_set_timestamp"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting current validator set timestamp: {e}")
-        return None
+        return None, e
 
-def get_validator_set_index_by_timestamp(timestamp):
+def get_validator_set_index_by_timestamp(timestamp) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_validator_set_index_by_timestamp/{timestamp}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting validator set index by timestamp: {e}")
-        return None
+        return None, e
 
 # oracle data functions 
-def get_data_before(query_id, timestamp_before):
+def get_data_before(query_id, timestamp_before) -> (dict, Exception):
     query_id = strip_0x(query_id)
     request = f"{rpc_endpoint}/tellor-io/layer/oracle/get_data_before/{query_id}/{timestamp_before}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting data before: {e}")
-        return None
+        return None, e
 
-def get_snapshots_by_report(query_id, timestamp):
+def get_snapshots_by_report(query_id, timestamp) -> (dict, Exception):
     query_id = strip_0x(query_id)
     request = f"{rpc_endpoint}/layer/bridge/get_snapshots_by_report/{query_id}/{timestamp}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting snapshots by report: {e}")
-        return None
+        return None, e
 
-def get_attestations_by_snapshot(snapshot):
+def get_attestations_by_snapshot(snapshot) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_attestations_by_snapshot/{snapshot}"
     try:
         response = requests.get(request)
-        return response.json()
+        return response.json(), None
     except Exception as e:
         print(f"layer_client: Error getting attestations by snapshot: {e}")
-        return None
+        return None, e
 
 def get_attestation_data_by_snapshot(snapshot) -> (dict, Exception):
     request = f"{rpc_endpoint}/layer/bridge/get_attestation_data_by_snapshot/{snapshot}"
@@ -109,12 +109,22 @@ def query_latest_oracle_data(query_id) -> (dict, Exception):
     # subtract 1 second to account for attestation time,
     # TODO: optimize
     current_time = int(time.time()) * 1000 - 1000 
-    report = get_data_before(query_id, current_time)
-    snapshots = get_snapshots_by_report(query_id, report["timestamp"])
+    report, e = get_data_before(query_id, current_time)
+    if e:
+        return None, e
+    snapshots, e = get_snapshots_by_report(query_id, report["timestamp"])
+    if e:
+        return None, e
     last_snapshot = snapshots["snapshots"][-1]
-    attestations = get_attestations_by_snapshot(last_snapshot)
-    attestation_data = get_attestation_data_by_snapshot(last_snapshot)
-    current_validator_set = get_current_validator_set()
+    attestations, e = get_attestations_by_snapshot(last_snapshot)
+    if e:
+        return None, e
+    attestation_data, e = get_attestation_data_by_snapshot(last_snapshot)
+    if e:
+        return None, e
+    current_validator_set, e = get_current_validator_set()
+    if e:
+        return None, e
     oracle_proof = {
         "attestations": attestations,
         "attestation_data": attestation_data,
@@ -124,22 +134,38 @@ def query_latest_oracle_data(query_id) -> (dict, Exception):
     return oracle_proof, None
 
 def get_next_validator_set_timestamp(given_timestamp) -> (str, Exception):
-    given_ts_index = get_validator_set_index_by_timestamp(given_timestamp).get("index")
-    next_ts_index = str(int(given_ts_index) + 1)
-    next_ts = get_validator_timestamp_by_index(next_ts_index).get("timestamp")
-    return next_ts, None
+    given_ts_index, e = get_validator_set_index_by_timestamp(given_timestamp)
+    if e:
+        return None, e
+    next_ts_index = str(int(given_ts_index.get("index")) + 1)
+    next_ts, e = get_validator_timestamp_by_index(next_ts_index)
+    if e:
+        return None, e
+    return next_ts.get("timestamp"), None
 
 def get_previous_validator_set_timestamp(given_timestamp) -> (str, Exception):
-    given_ts_index = get_validator_set_index_by_timestamp(given_timestamp).get("index")
-    previous_ts_index = str(int(given_ts_index) - 1)
-    previous_ts = get_validator_timestamp_by_index(previous_ts_index).get("timestamp")
-    return previous_ts, None
+    given_ts_index, e = get_validator_set_index_by_timestamp(given_timestamp)
+    if e:
+        return None, e
+    previous_ts_index = str(int(given_ts_index.get("index")) - 1)
+    previous_ts, e = get_validator_timestamp_by_index(previous_ts_index)
+    if e:
+        return None, e
+    return previous_ts.get("timestamp"), None
 
 def query_validator_set_update(given_timestamp) -> (dict, Exception):
-    valset_sigs = get_valset_sigs(given_timestamp)
-    valset_checkpoint = get_validator_checkpoint_params(given_timestamp)
-    previous_timestamp = get_previous_validator_set_timestamp(given_timestamp)
-    previous_valset = get_valset_by_timestamp(previous_timestamp)
+    valset_sigs, e = get_valset_sigs(given_timestamp)
+    if e:
+        return None, e
+    valset_checkpoint, e = get_validator_checkpoint_params(given_timestamp)
+    if e:
+        return None, e
+    previous_timestamp, e = get_previous_validator_set_timestamp(given_timestamp)
+    if e:
+        return None, e
+    previous_valset, e = get_valset_by_timestamp(previous_timestamp)
+    if e:
+        return None, e
     valset_update_params = {
         "valset_sigs": valset_sigs,
         "valset_checkpoint": valset_checkpoint,
@@ -148,14 +174,26 @@ def query_validator_set_update(given_timestamp) -> (dict, Exception):
     return valset_update_params, None
 
 def get_blobstream_init_params() -> (dict, Exception):
-    first_timestamp = get_validator_timestamp_by_index(0).get("timestamp")
-    checkpoint_params = get_validator_checkpoint_params(first_timestamp)
+    first_timestamp, e = get_validator_timestamp_by_index(0)
+    if e:
+        return None, e
+    checkpoint_params, e = get_validator_checkpoint_params(first_timestamp.get("timestamp"))
+    if e:
+        return None, e
     return checkpoint_params, None
 
 def get_layer_latest_validator_timestamp() -> (str, Exception):
-    return get_current_validator_set_timestamp().get("timestamp"), None
+    latest_timestamp, e = get_current_validator_set_timestamp()
+    if e:
+        return None, e
+    return latest_timestamp.get("timestamp"), None
 
 def get_current_validator_set() -> (dict, Exception):
-    latest_timestamp = get_layer_latest_validator_timestamp()
-    return get_valset_by_timestamp(latest_timestamp), None
+    latest_timestamp, e = get_layer_latest_validator_timestamp()
+    if e:
+        return None, e
+    valset, e = get_valset_by_timestamp(latest_timestamp)
+    if e:
+        return None, e
+    return valset, None
 
--- relayer.py
+++ relayer.py
@@ -1,11 +1,11 @@
-from layer_client import query_validator_set_update, query_latest_oracle_data, get_validator_timestamp_by_index, get_blobstream_init_params, get_layer_latest_validator_timestamp, get_next_validator_set_timestamp
+from layer_client import query_validator_set_update, query_latest_oracle_data, get_blobstream_init_params, get_layer_latest_validator_timestamp, get_next_validator_set_timestamp
 from evm_client import init_web3, get_blobstream_validator_timestamp, init_blobstream, update_validator_set, get_current_price_data_timestamp, update_oracle_data
 from transformer import transform_blobstream_init_params, transform_valset_update_params, transform_oracle_update_params
 import time
 
 blobstream_contract_address = "0x0000000000000000000000000000000000000000"
 query_id = "0x83a7f3d48786ac2667503a61e8c415438ed2922eb86a2906e4ee66d9a2ce4992" # eth/usd
-sleep_time = 10
+sleep_time = 5
 
 def start_relayer():
     print("relayer: Starting relayer...")
@@ -17,33 +17,51 @@ def start_relayer():
 
     if blobstream_validator_timestamp == 0:
         print("relayer: Initializing Blobstream...")
-        blobstream_init()
-        blobstream_validator_timestamp = get_blobstream_validator_timestamp()
+        e = blobstream_init()
+        if e:
+            print("relayer: Error initializing Blobstream: ", e)
+            return
 
     while True:
-        layer_validator_timestamp = get_layer_latest_validator_timestamp()
+        time.sleep(sleep_time)
+        layer_validator_timestamp, e = get_layer_latest_validator_timestamp()
+        if e:
+            print("relayer: Error getting latest Layer validator timestamp: ", e)
+            continue
         print("relayer: Layer validator timestamp: ", layer_validator_timestamp)
-
+        blobstream_validator_timestamp = get_blobstream_validator_timestamp()
+        print("relayer: Blobstream validator timestamp: ", blobstream_validator_timestamp)
         if int(blobstream_validator_timestamp) < int(layer_validator_timestamp):
             print("relayer: Updating to latest Layer validator set...")
-            update_to_latest_layer_validator_set(blobstream_validator_timestamp, layer_validator_timestamp)
+            e = update_to_latest_layer_validator_set(blobstream_validator_timestamp, layer_validator_timestamp)
+            if e:
+                print("relayer: Error updating to latest Layer validator set: ", e)
+                continue
+        e = update_user_oracle_data(query_id)
+        if e:
+            print("relayer: Error updating user oracle data: ", e)
+            continue
 
-        update_user_oracle_data(query_id)
-        time.sleep(sleep_time)
-
-def blobstream_init():
-    print("Initializing Blobstream...")
-    checkpoint_params = get_blobstream_init_params()
+def blobstream_init() -> Exception:
+    print("relayer: Initializing Blobstream...")
+    checkpoint_params, e = get_blobstream_init_params()
+    if e:
+        return e
     print("relayer: Checkpoint params: ", checkpoint_params)
     init_tx_params = transform_blobstream_init_params(checkpoint_params)
     print("relayer: Init tx params: ", init_tx_params)
     init_tx = init_blobstream(init_tx_params)
     print("relayer: Init tx: ", init_tx)
+    return None
 
-def update_to_latest_layer_validator_set(blobstream_validator_timestamp, layer_validator_timestamp):
+def update_to_latest_layer_validator_set(blobstream_validator_timestamp, layer_validator_timestamp) -> Exception:
     while int(blobstream_validator_timestamp) < int(layer_validator_timestamp):
-        next_validator_timestamp = get_next_validator_set_timestamp(blobstream_validator_timestamp)
-        valset_update_params = query_validator_set_update(next_validator_timestamp)
+        next_validator_timestamp, e = get_next_validator_set_timestamp(blobstream_validator_timestamp)
+        if e:
+            return e
+        valset_update_params, e = query_validator_set_update(next_validator_timestamp)
+        if e:
+            return e
         print("relayer: Valset update params: ", valset_update_params)
         valset_update_tx_params = transform_valset_update_params(valset_update_params)
         print("relayer: Valset update tx params: ", valset_update_tx_params)
@@ -53,10 +71,13 @@ def update_to_latest_layer_validator_set(blobstream_validator_timestamp, layer_v
         print("relayer: Blobstream validator timestamp: ", blobstream_validator_timestamp)
 
     print("relayer: Blobstream valset up to date with Layer valset")
-    
-def update_user_oracle_data(query_id):
+    return None
+
+def update_user_oracle_data(query_id) -> Exception:
     print("relayer: Updating oracle data...")
-    oracle_proof = query_latest_oracle_data(query_id)
+    oracle_proof, e = query_latest_oracle_data(query_id)
+    if e:
+        return e
     current_price_data_timestamp = get_current_price_data_timestamp()
     print("relayer: Current price data timestamp: ", current_price_data_timestamp)
     print("relayer: Oracle proof: ", oracle_proof)
@@ -65,6 +86,9 @@ def update_user_oracle_data(query_id):
         oracle_update_tx_params = transform_oracle_update_params(oracle_proof)
         print("relayer: Oracle update tx params: ", oracle_update_tx_params)
         update_oracle_data(oracle_update_tx_params)
-        print("Oracle data updated")
+        print("relayer: Oracle data updated")
+    else:
+        print("relayer: No new oracle data available")
+    return None
 
 start_relayer()
